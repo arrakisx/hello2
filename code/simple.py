af5e22a2aec33a9bae38f1fae3c001b5547f2ca3
Michał Zientak <mzie@candf.com>
2018-05-22 08:25:54 +0200

    Added author to simple.py

--- code/simple.py
+++ code/simple.py
@@ -1,3 +1,5 @@
+#Author: Michał
+#Date created: 2018-05-22
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print "iteration {iteration} is {name}".format(iteration=i, name=name)
